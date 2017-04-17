c10a4132de5834716c3e98fa52295b7bf58b768b
Taras Prezlyata <Taras Prezlyata>
2017-04-17 12:41:52 +0300

    13 - Connecting to the Database

--- music/urls.py
+++ music/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     # /music/
     url(r'^$', views.index, name='index'),
 
-    # /music/71/
+    # /music/712/
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
 
 ]
